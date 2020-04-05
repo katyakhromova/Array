ad2a5f6c3d3eca7733bcb17d6b2f48e90c3ac222
katyakhromova <62327452+katyakhromova@users.noreply.github.com>
2020-04-05 16:03:38 +0300

    Add files via upload

--- Array/src/Second.java
+++ Array/src/Second.java
@@ -1,5 +1,8 @@
 import static src.Lesson.secondMethod;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Second {
     public static void main(String[] args) {
         secondMethod();
@@ -8,6 +11,15 @@ public class Second {
         /*Для выполнения задания понадобиться Scanner s = new Scanner(System.in);
         Необходимо ввести 10 значений с клавиатуры  и сохранить их в array, после этого вывести на экран.*/
 
-
+        Scanner k = new Scanner(System.in);
+        System.out.println("Введите 10 целых чисел для массива: ");
+        int array[] = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = k.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
+
